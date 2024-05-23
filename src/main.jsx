917766ae05d203167975633683a010a1c06e3117
Иванов Алексей Владимирович <alvl.ivanov@samolet.ru>
2024-05-24 01:36:53 +0300

    step2: add fixtures

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import ReactDOM from 'react-dom/client';
 import Widget from '@hexlet/chatbot-v2';
-import steps from '@hexlet/chatbot-v2/example-steps';
+import steps from '../__fixtures__/1.json';
 import '@hexlet/chatbot-v2/styles';
 
 const container = document.getElementById('root');
