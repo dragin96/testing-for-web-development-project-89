0a36d0a88c7baba47d7d5adcfa766d0ec2609bcc
Иванов Алексей Владимирович <alvl.ivanov@samolet.ru>
2024-06-10 23:10:19 +0300

    step: 5 add react app

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,6 @@
+import App from './App';
 import ReactDOM from 'react-dom/client';
-import '@hexlet/chatbot-v2/styles';
-import App from "./App.jsx";
 
 const container = document.getElementById('root');
 ReactDOM.createRoot(container)
-    .render(App());
+    .render(<App/>);
